772d4624697294ce31ed00781ffb1eefa76307b7
Hugo Lu <hugolu87@gmail.com>
2023-02-23 21:46:28 +0000

    Some more nice changes

--- macros/regression/regression.sql
+++ macros/regression/regression.sql
@@ -4,19 +4,48 @@
                     predictions_table_name,
                     predictions_variables,
                     predictions_run_id,
-                    predictions_value_name) -%}
+                    predictions_run_time,
+                    predictions_value_name,
+                    materialize ='table',
+                    window = 'day',
+                    duration= 1) -%}
 
 -- Macro to consolidate actuals to predictions at any level of granularity
 -- i.e. one prediction per combination of n variables. For example,
 -- usage of an api for different api clients on different days
 
+{% if materialize == 'incremental' %}
+
+
+    {{config(
+        materialized='incremental',
+        unique_key ='_pk'
+    )}}
+
+
+{% elif materialize == 'view' %}
+
+    {{config(
+        materialized='view',
+        unique_key ='_pk'
+    )}}
+
+{% else %}
+
+    {{config(
+        materialized='table',
+        unique_key ='_pk'
+    )}}
+
+
+{% endif %}
+
 actuals as (
     select
-
-    {% for val in actuals_variables %}
-    {{val}} as  {{val}},
-    {% endfor %}
-    {{actuals_value_name}} actuals_value
+        {% for val in actuals_variables %}
+        {{val}} as  {{val}},
+        {% endfor %}
+        {{actuals_value_name}} actuals_value
     from {{actuals_table_name }}
 ),
 predictions as (
@@ -24,8 +53,8 @@ predictions as (
         {% for val in predictions_variables %}
         {{val}} as  {{val}},
         {% endfor %}
-
         {{predictions_run_id}} predictions_run_id,
+        {{predictions_run_time}} predictions_run_time,
         {{predictions_value_name}} predictions_value
     from {{predictions_table_name}}
 )
@@ -33,6 +62,12 @@ select
 
     predictions.*,
     actuals.actuals_value,
+    sha2_binary(concat(
+       {% for val in predictions_variables %}
+       ifnull(cast(predictions.{{val}} as string), 'null'),
+       {% endfor %}
+       ifnull(cast(predictions.predictions_run_id as string), 'null')
+       )) _pk,
        (actuals.actuals_value - predictions.predictions_value) abs_error,
        div0((actuals.actuals_value - predictions.predictions_value), actuals.actuals_value) pct_error
 
@@ -47,4 +82,8 @@ on
 
 {% endfor %}
 where predictions.predictions_value is not null and lower(predictions.predictions_value) != 'nan'
+{% if is_incremental() %}
+  -- this filter will only be applied on an incremental run
+  and predictions_run_time >= dateadd({{window}}, -{{duration}}, current_date())
+{% endif %}
 {% endmacro %}
--- models/regression/regression.sql
+++ models/regression/regression.sql
@@ -3,7 +3,15 @@
 with actuals_staging as (
     select
         date,
-        product,
+        case when product = 'Banking API' then 'BANKING_ACC'
+        when product = 'Other' then 'x'
+        when product = 'Accounting API' then 'ACCOUNTING_ACC'
+        when product = 'Commerce API' then 'COMMERCE_ACC'
+        when product = 'Sync for Expenses' then 'SYNC_EXP_ACC'
+        when product = 'Bank Feeds' then 'x'
+        when product = 'Assess' then 'ASSESS_ACC'
+        when product = 'Sync for Commerce' then 'SYNC_COMM_ACC'
+        else null end product,
         count(distinct companyguid) companies
 
     from codat.public_aggregated.codat_active_companies_aggregated
@@ -15,7 +23,9 @@ with actuals_staging as (
 
 {{regression(
 'actuals_staging', var('actuals_variables'), 'companies',
-  'ds_product_acc_forecast_v2', var('predictions_variables'), 'RUN_DATE', 'VALUE'
+'ds_product_acc_forecast_v2', var('predictions_variables'), 'RUN_DATE', 'RUN_DATE', 'VALUE', materialize = 'incremental',
+
+  duration = 30
 )}}
 
 
