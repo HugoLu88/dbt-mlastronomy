c67bfa3c6a1adc3788f0a2d51504b99541da0280
Hugo Lu <hugolu87@gmail.com>
2023-02-25 09:27:21 +0000

    updated aggregared macro

--- macros/regression/regression_aggregated.sql
+++ macros/regression/regression_aggregated.sql
@@ -32,19 +32,55 @@
 
 
 {% endif %}
-
+with
 {% for item in tables_ %}
-    select *,
-      '{{item}}' table_name
+ staging_{{item}} as (
+
+    select
+
+    predictions_run_id,
+    predictions_run_time,
+    predictions_table_name,
+    {{mean_absolute_error('abs_error')}} mean_absolute_error,
+    {{mean_squared_error('abs_error')}} mean_squared_error,
+    {{root_mean_squared_error('abs_error')}} root_mean_squared_error,
+    {{mean_absolute_error('pct_error')}} mean_pct_error,
+    {{median_absolute_error('pct_error')}} median_pct_error,
+    {{total_sum_of_squares('squares')}} tss,
+    {{residual_sum_of_squares('abs_error')}} rss,
+    {{regression_coefficient('tss','rss')}} regression_coefficient
+
+
     from {{ref(item)}}
     {% if is_incremental() %}
       -- this filter will only be applied on an incremental run
     where predictions_run_time >= dateadd({{window}}, -{{duration}}, current_date())
     {% endif %}
-    {% if not loop.last %}
-    union all
-    {% endif %}
+    group by 1,2,3
+
+
+),
+
 {% endfor %}
+
+aggregated as (
+    {% for item in tables_ %}
+        select
+            *
+        from staging_{{item}}
+
+        {% if not loop.last %}
+                union all
+        {% endif %}
+    {% endfor %}
+
+    )
+
+
+    select
+    *
+    from aggregated
+
 
 
 {% endmacro %}
